7ca6cf4594efe70fced002ca49cd90db33ed4f0d
Muvvalakaran <karanmuvvala@yahoo.in>
2022-08-06 20:20:54 -0600

    Added code to symbolic transition system for each action from PDDL file

--- main.py
+++ main.py
@@ -1,143 +1,262 @@
+from fileinput import filename
 import os
 import sys
+import copy
+from tabnanny import verbose
 
-from dd.autoref import BDD
+# from dd.autoref import BDD
+from bidict import bidict
 from src.causal_graph import CausalGraph
 from src.two_player_game import TwoPlayerGame
 from src.transition_system import FiniteTransitionSystem
 
+from typing import Tuple, List
+from functools import reduce
 
-def test_constraint_vs_explicit_transition_thr():
+import graphviz as gv
+import math
+from cudd import Cudd
+from itertools import count, product
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+class SymbolicTransitionSystem(object):
     """
-    A method to  test my speculation on the elevator reachability problem
-
-    Floor 0: 00 (!x0.!x1)
-    Floor 2: 01 (!x0.x1)
-    Floor 1: 10 (x0.!x1)
-    Floor 3: 11 (x0.x1)
-
-    We use prime to represent the next state we can transit to.
-
-    Floor 0: 00 (!x0'.!x1')
-    Floor 2: 01 (!x0'.x1')
-    Floor 1: 10 (x0'.!x1')
-    Floor 3: 11 (x0'.x1')
-
+    A class to construct a symbolic transition system for each operator - in our case Action 
     """
-    # instantiate a manger
-    bdd = BDD()
-    # bdd.declare('x')
-    # bdd.add_expr(r'~ x')
-    # bdd.dump("single_var.pdf")
-    # sys.exit(-1)
+
+    def __init__(self, curr_states: list , next_states: list, task, domain, manager):
+        self.sym_vars_curr = curr_states
+        self.sym_vars_next = next_states
+        self.init: frozenset = task.initial_state
+        self.goal: frozenset = task.goals
+        self.facts:dict = task.facts
+        self.task: dict = task
+        self.domain: dict = domain
+        self.manager = manager
+        self.actions: dict = domain.actions
+        self.tr_action_idx_map: dict = {}
+        self.sym_tr_actions: list = []
+        self.predicate_sym_map_curr: bidict = {}
+        self.predicate_sym_map_nxt: bidict = {}
+        self._create_sym_var_map()
+        self._initialize_bdds_for_actions()
+
+    
+    def _initialize_bdds_for_actions(self):
+        """
+        A function to intialize bdds for all the actions
+        """
+        #  initiate BDDs for all the action 
+        action_idx_map = {}
+        _actions = list(self.actions.keys())
+        for _idx, _action in enumerate(_actions):
+            action_idx_map[_action] = _idx
+        
+        self.tr_action_idx_map = action_idx_map
+        self.sym_tr_actions = [self.manager.bddZero() for _ in range(len(self.actions))]
+
+    
+    def build_actions_tr_func(self, curr_edge_action: str):
+        """
+        A function to build a symbolic transition function corresponding to each action 
+        """
+
+        # flag for debugging - checking if the # of symbolic edges is same as the number of explicit edges
+        added_edge: bool = False
+
+        _actions = list(self.actions.keys())
+        # since pre, post condition are all forzenset, we iterate over it
+        pre_conds = tuple(curr_edge_action.preconditions)
+        add_effects = tuple(curr_edge_action.add_effects)
+        del_effects = tuple(curr_edge_action.del_effects)
+
+        # get the bool formula for the above predicates
+        pre_list = [self.predicate_sym_map_curr.get(pre_cond) for pre_cond in pre_conds]
+        add_list = [self.predicate_sym_map_nxt.get(add_effect) for add_effect in add_effects]
+        del_list = [self.predicate_sym_map_nxt.get(del_effect) for del_effect in del_effects]
+
+        if len(pre_list) != 0:
+            pre_sym = reduce(lambda a, b: a & b, pre_list)
+        else:
+            pre_sym = self.manager.bddOne()
+        if len(add_list) != 0:
+            add_sym = reduce(lambda a, b: a & b, add_list)
+        else:
+            add_sym = self.manager.bddOne()
+        if len(del_list) != 0:
+            del_sym = reduce(lambda a, b: a & b, del_list)
+        else:
+            del_sym = self.manager.bddZero()
+        
+        _curr_action_name = curr_edge_action.name
+
+        # instead looking for the action, extract it (action name)
+        _action = _curr_action_name.split()[0]
+        _action = _action[1:]   # remove the intial '(' braket
+
+        # assert that its a valid name
+        assert _action in _actions, "FIX THIS: Failed extracting a valid action."
+
+        # for _a in _actions:
+        #     if _a in _curr_action_name:
+        # if _action == 'transfer':
+        #     print('I am trying to transfer an object!')
+        # if curr_fact in curr_edge_action.preconditions and next_fact in curr_edge_action.add_effects:
+            # find it corresponding index in transtion function list 
+        _idx = self.tr_action_idx_map.get(_action)
+        self.sym_tr_actions[_idx] |= pre_sym & add_sym & ~del_sym
+        added_edge = True
+            # break
+        
+        # break
+        
+        return _action
 
 
-    # declare variables
-    bdd.declare("x0", "x0'", "x1", "x1'")
-    # TLA+ syntax
-    # only adding the constraints - the set of valid assignments correspond to the set of valid transitions
-    s = (
-        r"((~ x0 /\ ~ x1) => ( (~ x0' /\ ~ x1') \/ (x0' /\ ~ x1') )) /\ "
-        r"((x0 /\ ~ x1) => ~ (x0' /\ x1')) /\ "
-        r"((~ x0 /\ x1) => ( (~ x0' /\ x1') \/ (x0' /\ ~ x1') )) /\ "
-        r" ~ (x0 /\ x1)")
+    
+    def _create_sym_var_map(self):
+        """
+        Loop through all the facts that are reachable and assign a boolean funtion to it
+        """
 
-    # explicitly stating the transitions
-    s_expl = r"(!x0 & !x1 & x0' & !x1') |" \
-             r" (!x0 & x1 & x0' & !x1') |" \
-             r" (x0 & !x1 & !x0' & x1') |" \
-             r" (x0 & !x1 & !x0' & !x1')"
-    transitions = bdd.add_expr(s_expl)
+        # create all combinations of 1-true and 0-false
+        boolean_str = list(product([1, 0], repeat=len(self.sym_vars_curr)))
 
-    # print(transitions.negated)
+        _node_int_map_curr = bidict({state: boolean_str[index] for index, state in enumerate(self.facts)})
+        _node_int_map_next = copy.deepcopy(_node_int_map_curr)
 
-    # to count the # of satifiable assignments and the assignmet that lead to valid transition
-    print(bdd.count(~ transitions))
-    print(list(bdd.pick_iter(~ transitions)))
-    # bdd.dump("transition_constrant_expl_negated.pdf", roots=[~ transitions])
+        assert len(boolean_str) >= len(_node_int_map_next), "FIX THIS: Looks like there are more Facts that boolean variables!"
+
+        # loop over all the boolean strings and convert them respective bdd vars
+        for _key, _value in _node_int_map_curr.items():
+            _curr_val_list = []
+            _next_val_list = []
+            # _bool_fun = self.manager.bddOne()
+            for _idx, _ele in enumerate(_value):
+                if _ele == 1:
+                    # _bool_func = _bool_fun & self.sym_vars_curr[_idx]
+                    _curr_val_list.append(self.sym_vars_curr[_idx])
+                    _next_val_list.append(self.sym_vars_next[_idx])
+                else:
+                    _curr_val_list.append(~self.sym_vars_curr[_idx])
+                    _next_val_list.append(~self.sym_vars_next[_idx])
+            
+            _bool_func_curr = reduce(lambda a, b: a & b, _curr_val_list)
+            _bool_func_nxt = reduce(lambda a, b: a & b, _next_val_list)
+
+            # update bidict accordingly
+            _node_int_map_curr[_key] = _bool_func_curr
+            _node_int_map_next[_key] = _bool_func_nxt    
+        
+        self.predicate_sym_map_curr = _node_int_map_curr
+        self.predicate_sym_map_nxt = _node_int_map_next
+    
+
+    def create_transition_system(self, verbose: bool = False, plot: bool = False):
+        """
+        A function to create the TR function for each Action we hav defined in the domain
+        """
+        if verbose:
+            print(f"Creating TR for Actions {self.domain.actions}")
+        
+        scheck_count_transit = 0
+        scheck_count_transfer = 0
+        scheck_count_grasp = 0
+        scheck_count_release = 0
+        scheck_count_hmove = 0
+        
+        for _action in self.task.operators:
+            edge_name = self.build_actions_tr_func(curr_edge_action=_action)
+                
+            if edge_name == 'transit':
+                scheck_count_transit += 1
+            elif edge_name == 'transfer':
+                scheck_count_transfer += 1
+            elif edge_name == 'grasp':
+                scheck_count_grasp += 1
+            elif edge_name == 'release':
+                scheck_count_release += 1
+            elif edge_name == 'human-move':
+                scheck_count_hmove += 1
+            else:
+                print("Invalid action!!!!!")
+                sys.exit(-1)
 
 
-def build_symbolic_model(transition_sys_graph: TwoPlayerGame):
+        for _action, _idx in self.tr_action_idx_map.items():
+            print(f"Charateristic Function for action {_action} \n")
+            print(self.sym_tr_actions[_idx], " \n")
+            if plot:
+                file_path = PROJECT_ROOT + f'/plots/{_action}_trans_func.dot'
+                file_name = PROJECT_ROOT + f'/plots/{_action}_trans_func.pdf'
+                self.manager.dumpDot([self.sym_tr_actions[_idx]], file_path=file_path)
+                # gv.draw('test.png')
+                gv.render(engine='dot', format='pdf', filepath=file_path, outfile=file_name)
+                # gv.Source.from_file(file_path).view()
+
+        
+        print("# of total symbolic transfer edge counts", scheck_count_transit)
+        print("# of total symbolic transit edge counts", scheck_count_transfer)
+        print("# of total symbolic grasp edge counts", scheck_count_grasp)
+        print("# of total symbolic release edge counts", scheck_count_release)
+        print("# of total symbolic human-move edge counts", scheck_count_hmove)
+        print("All done!")
+        
+
+def create_symbolic_vars(num_of_facts: int, manager) -> Tuple[list, list]:
     """
-    A helper function to create a symbolic graph
+    A helper function to create log⌈num_of_facts⌉ 
     """
-    # bdd = BDD()
-    # bdd.declare('x', 'y', 'z')
-    # u = bdd.add_expr(r'x /\ y')
-    #
-    # values = dict(x=False, y=True)
-    # v = bdd.let(values, u)
-    # print(v)
-    # # for d in bdd.pick_iter(u):
-    # #     print(d)
-    #
-    # # for _ in range(1000):
-    # #     d = bdd.pick(u, care_vars=['x', 'y'])
-    # #     print(d)
-    # bdd.collect_garbage()  # optional
-    # bdd.dump('awesome.pdf')
+    curr_state_vars: list = []
+    next_state_vars: list = []
 
-    # sys.exit(-1)
+    for num_var in range(math.ceil(math.log2(num_of_facts))):
+        # if num_var % 2 == 0:
+            curr_state_vars.append(manager.bddVar(2*num_var, f'x{num_var}'))
+            next_state_vars.append(manager.bddVar(2*num_var + 1, f'y{num_var}'))
+        # else: 
 
-    # relabel all nodes
-    new_graph = transition_sys_graph.internal_node_mapping(transition_sys_graph.transition_system)
+    return (curr_state_vars, next_state_vars)
 
-    # create a binary representation for each variable
-    bdd = BDD()
-    for _n in new_graph._graph.nodes():
-        # _n = _n.replace(" ", "")
-        new_str = "n" + str(_n)
-        bdd.declare(new_str)
+def create_symbolic_causal_graph(cudd_manager):
+    
 
-    # create a bdd for each transition. The expression is in TLA+ - a formal specification language
-    trans_bdd_str_list = []
-    counter = 0
-    for _e in new_graph._graph.edges():
-        # get the corresponding vars
-        pre_node_var = bdd.level_of_var("n" + str(_e[0]))
-        post_node_var = bdd.level_of_var("n" + str(_e[1]))
-
-        # create bdd transition str
-        tran_str = r'({u} & {v})'.format(u="n" + str(_e[0]),
-                                         v="n" + str(_e[1]))
-        trans_bdd_str_list.append(tran_str)
-
-        # create a bdd for this transition
-        # trans_bdd_list.append(bdd.add_expr(r'({u} & {v})'.format(u="n" + str(_e[0]),
-        #                                                           v="n" + str(_e[1]))))
-        #
-        if counter == 1:
-        #
-        #     d = dict(n0=trans_bdd_list[1])
-        #     f = bdd.let(d, trans_bdd_list[0])
-        #     bdd.dump('transition.pdf')
-        #
-        #     for eval in bdd.pick_iter(f):
-        #         print(eval)
-        #
-            break
-        #
-        counter += 1
-
-    str_expr = ' | '.join(trans_bdd_str_list)
-    print(str_expr)
-
-    bdd.add_expr(str_expr)
-    # bdd.collect_garbage()  # optional
-    bdd.dump('transition_function.pdf')
-
-    sys.exit(-1)
-
-
-def get_graph(print_flag: bool = False):
-    _project_root = os.path.dirname(os.path.abspath(__file__))
-
-    _domain_file_path = _project_root + "/pddl_files/two_table_scenario/arch/domain.pddl"
-    _problem_file_path = _project_root + "/pddl_files/two_table_scenario/arch/problem.pddl"
+    _domain_file_path = PROJECT_ROOT + "/pddl_files/two_table_scenario/arch/domain.pddl"
+    _problem_file_path = PROJECT_ROOT + "/pddl_files/two_table_scenario/arch/problem.pddl"
 
     _causal_graph_instance = CausalGraph(problem_file=_problem_file_path,
                                          domain_file=_domain_file_path,
                                          draw=False)
+    
+    _causal_graph_instance.build_causal_graph(add_cooccuring_edges=False, relabel=False)
+    print("No. of edges in the graph:", len(_causal_graph_instance.causal_graph._graph.edges()))
+
+    # problem = _causal_graph_instance.problem
+
+    task_facts = _causal_graph_instance.task.facts
+    task_goals = _causal_graph_instance.task.goals
+    task_init = _causal_graph_instance.task.initial_state
+
+    
+    # the number of boolean variables (|x|) = log⌈|facts|⌉ - Because facts represent all possible predicates in our causal graph 
+    curr_state, next_state = create_symbolic_vars(num_of_facts=len(task_facts),
+                                                  manager=cudd_manager)
+
+
+    return _causal_graph_instance.task, _causal_graph_instance.problem.domain, curr_state, next_state, 
+
+
+def get_graph(print_flag: bool = False):
+
+    _domain_file_path = PROJECT_ROOT + + "/pddl_files/two_table_scenario/arch/domain.pddl"
+    _problem_file_path = PROJECT_ROOT + + "/pddl_files/two_table_scenario/arch/problem.pddl"
+
+    _causal_graph_instance = CausalGraph(problem_file=_problem_file_path,
+                                         domain_file=_domain_file_path,
+                                         draw=True)
 
     _causal_graph_instance.build_causal_graph(add_cooccuring_edges=False, relabel=False)
 
@@ -147,24 +266,28 @@ def get_graph(print_flag: bool = False):
         print(
             f"No. of edges in the Causal Graph is :{len(_causal_graph_instance._causal_graph._graph.edges())}")
 
-    _transition_system_instance = FiniteTransitionSystem(_causal_graph_instance)
-    _transition_system_instance.build_transition_system(plot=False, relabel_nodes=False)
-    _transition_system_instance.build_arch_abstraction(plot=False, relabel_nodes=False)
+    # _transition_system_instance = FiniteTransitionSystem(_causal_graph_instance)
+    # _transition_system_instance.build_transition_system(plot=False, relabel_nodes=False)
+    # _transition_system_instance.build_arch_abstraction(plot=False, relabel_nodes=False)
 
-    if print_flag:
-        print(f"No. of nodes in the Transition System is :"
-              f"{len(_transition_system_instance.transition_system._graph.nodes())}")
-        print(f"No. of edges in the Transition System is :"
-              f"{len(_transition_system_instance.transition_system._graph.edges())}")
+    # if print_flag:
+    #     print(f"No. of nodes in the Transition System is :"
+    #           f"{len(_transition_system_instance.transition_system._graph.nodes())}")
+    #     print(f"No. of edges in the Transition System is :"
+    #           f"{len(_transition_system_instance.transition_system._graph.edges())}")
 
-    return _transition_system_instance
+    # return _transition_system_instance
 
 
 if __name__ == "__main__":
-    # testing BDD theory
-    # test_constraint_vs_explicit_transition_thr()
-    # sys.exit(-1)
+ 
 
     # construct a sample rwo player game and wrap it to construct its symbolic version
-    transition_graph = get_graph(print_flag=True)
-    build_symbolic_model(transition_graph)
+    # transition_graph = get_graph(print_flag=True)
+    # build_symbolic_model(transition_graph)
+
+    cudd_manager = Cudd()
+    task, domain, curr_state, next_state = create_symbolic_causal_graph(cudd_manager=cudd_manager)
+
+    sym_tr = SymbolicTransitionSystem(curr_states=curr_state, next_states=next_state, task=task, domain=domain, manager=cudd_manager)
+    sym_tr.create_transition_system(verbose=False, plot=True)
