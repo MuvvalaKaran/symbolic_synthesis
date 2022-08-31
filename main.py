1b515e5d517a36f09bed235c1a139ab39332e5cf
Muvvalakaran <karanmuvvala@yahoo.in>
2022-08-30 21:37:46 -0600

    Added code to construct symbolic DFA

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import copy
-
 import graphviz as gv
 import math
 
@@ -17,14 +16,86 @@ from src.two_player_game import TwoPlayerGame
 from src.transition_system import FiniteTransitionSystem
 from graph_search import graph_search
 from src.symbolic_search import SymbolicSearch
-
-
-
+from src.two_player_game import TwoPlayerGame
 
 # from symbolic_planning.src.graph_search import forward_reachability
 
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+class SymbolicDFA(object):
+    """
+    A class to construct a symbolic transition system for each DFA 
+    """
+
+    def __init__(self, curr_states: list , next_states: list, dfa, manager, dfa_name):
+        self.sym_vars_curr = curr_states
+        self.sym_vars_next = next_states
+        self.manager = manager
+        self.dfa = dfa
+        self.dfa_name = dfa_name
+        self.dfa_bdd_tr = manager.bddZero()
+        self.predicate_sym_map_curr: bidict = {}
+        self.predicate_sym_map_nxt: bidict = {}
+        self._create_sym_var_map()
+
+
+    def _create_sym_var_map(self):
+        """
+        Loop through all the States that are reachable and assign a boolean funtion to it
+        """
+
+        # for dfa in self.dfa_list:
+        # create all combinations of 1-true and 0-false
+        boolean_str = list(product([1, 0], repeat=len(self.sym_vars_curr)))
+        
+        _node_int_map_curr = bidict({state: boolean_str[index] for index, state in enumerate(list(self.dfa._graph.nodes()))})
+        _node_int_map_next = copy.deepcopy(_node_int_map_curr)
+
+        assert len(boolean_str) >= len(_node_int_map_next), "FIX THIS: Looks like there are more Facts that boolean variables!"
+
+        # loop over all the boolean strings and convert them respective bdd vars
+        for _key, _value in _node_int_map_curr.items():
+            _curr_val_list = []
+            _next_val_list = []
+            for _idx, _ele in enumerate(_value):
+                if _ele == 1:
+                    _curr_val_list.append(self.sym_vars_curr[_idx])
+                    _next_val_list.append(self.sym_vars_next[_idx])
+                else:
+                    _curr_val_list.append(~self.sym_vars_curr[_idx])
+                    _next_val_list.append(~self.sym_vars_next[_idx])
+            
+            _bool_func_curr = reduce(lambda a, b: a & b, _curr_val_list)
+            _bool_func_nxt = reduce(lambda a, b: a & b, _next_val_list)
+
+            # update bidict accordingly
+            _node_int_map_curr[_key] = _bool_func_curr
+            _node_int_map_next[_key] = _bool_func_nxt    
+        
+        self.predicate_sym_map_curr = _node_int_map_curr
+        self.predicate_sym_map_nxt = _node_int_map_next
+    
+
+    def create_dfa_transition_system(self, verbose: bool = False, plot: bool = False):
+        """
+        A function to create the TR function for each Action we hav defined in the domain
+        """
+        for _curr, _nxt in self.dfa._graph.edges():
+            # get the boolean formula for the corresponding edge 
+            _curr_sym = self.predicate_sym_map_curr.get(_curr) 
+            _nxt_sym = self.predicate_sym_map_nxt.get(_nxt) 
+            
+            self.dfa_bdd_tr |= _curr_sym & _nxt_sym
+    
+        if verbose:
+            print(f"Charateristic Function for DFA  is \n")
+            print(self.dfa_bdd_tr, " \n")
+            if plot:
+                file_path = PROJECT_ROOT + f'/plots/{self.dfa_name}_trans_func.dot'
+                file_name = PROJECT_ROOT + f'/plots/{self.dfa_name}_trans_func.pdf'
+                self.manager.dumpDot([self.dfa_bdd_tr], file_path=file_path)
+                gv.render(engine='dot', format='pdf', filepath=file_path, outfile=file_name)
 
 
 class SymbolicTransitionSystem(object):
@@ -151,8 +222,8 @@ class SymbolicTransitionSystem(object):
         _action = _curr_action_name.split()[0]
         _action = _action[1:]   # remove the intial '(' braket
 
-        if _action == 'release':
-            print("Release the Kraken!")
+        # if _action == 'release':
+        #     print("Release the Kraken!")
 
         # assert that its a valid name
         assert _action in _actions, "FIX THIS: Failed extracting a valid action."
@@ -225,19 +296,19 @@ class SymbolicTransitionSystem(object):
         for _action in self.task.operators:
             edge_name = self.build_actions_tr_func(curr_edge_action=_action)
             
-            if edge_name == 'transit':
-                scheck_count_transit += 1
-            elif edge_name == 'transfer':
-                scheck_count_transfer += 1
-            elif edge_name == 'grasp':
-                scheck_count_grasp += 1
-            elif edge_name == 'release':
-                scheck_count_release += 1
-            elif edge_name == 'human-move':
-                scheck_count_hmove += 1
-            else:
-                print("Invalid action!!!!!")
-                sys.exit(-1)
+            # if edge_name == 'transit':
+            #     scheck_count_transit += 1
+            # elif edge_name == 'transfer':
+            #     scheck_count_transfer += 1
+            # elif edge_name == 'grasp':
+            #     scheck_count_grasp += 1
+            # elif edge_name == 'release':
+            #     scheck_count_release += 1
+            # elif edge_name == 'human-move':
+            #     scheck_count_hmove += 1
+            # else:
+            #     print("Invalid action!!!!!")
+            #     sys.exit(-1)
 
         if verbose:
             for _action, _idx in self.tr_action_idx_map.items():
@@ -258,26 +329,42 @@ class SymbolicTransitionSystem(object):
             print("All done!")
         
 
-def create_symbolic_vars(num_of_facts: int, manager) -> Tuple[list, list]:
+def create_symbolic_vars(num_of_facts: int, manager, curr_state_var_name: str = 'x', next_state_var_name: str = 'y') -> Tuple[list, list]:
     """
-    A helper function to create log⌈num_of_facts⌉ 
+    A helper function to create log⌈num_of_facts⌉
     """
     curr_state_vars: list = []
     next_state_vars: list = []
 
+    cur_state = curr_state_var_name
+    nxt_state = next_state_var_name
+
+
     for num_var in range(math.ceil(math.log2(num_of_facts))):
-        # if num_var % 2 == 0:
-            curr_state_vars.append(manager.bddVar(2*num_var, f'x{num_var}'))
-            next_state_vars.append(manager.bddVar(2*num_var + 1, f'y{num_var}'))
-        # else: 
+            curr_state_vars.append(manager.bddVar(2*num_var, f'{cur_state}{num_var}'))
+            next_state_vars.append(manager.bddVar(2*num_var + 1, f'{nxt_state}{num_var}'))
 
     return (curr_state_vars, next_state_vars)
+
+
+def create_symbolic_dfa_graph(cudd_manager, formula: str, dfa_num: int):
+    _two_player_instance = TwoPlayerGame(None, None)
+    _dfa = _two_player_instance.build_LTL_automaton(formula=formula, plot=False)
+    _state = _dfa.get_states()
+
+    # the number of boolean variables (|a|) = log⌈|DFA states|⌉
+    curr_state, next_state = create_symbolic_vars(num_of_facts=len(_state),
+                                                  manager=cudd_manager,
+                                                  curr_state_var_name=f'a{dfa_num}_',
+                                                  next_state_var_name=f'b{dfa_num}_')
+    return curr_state, next_state, _dfa
+
 
 def create_symbolic_causal_graph(cudd_manager, problem_pddl_file: str, domain_pddl_file: str):
     _causal_graph_instance = CausalGraph(problem_file=problem_pddl_file,
                                          domain_file=domain_pddl_file,
-                                         draw=False)
-    # _causal_graph_instance.build_causal_graph(add_cooccuring_edges=False, relabel=False)
+                                         draw=True)
+    _causal_graph_instance.build_causal_graph(add_cooccuring_edges=False, relabel=False)
     # print("No. of edges in the graph:", len(_causal_graph_instance.causal_graph._graph.edges()))
 
     task_facts = _causal_graph_instance.task.facts
@@ -321,34 +408,54 @@ def get_graph(print_flag: bool = False):
 
 
 if __name__ == "__main__":
- 
-
+    
+    BUILD_DFA: bool = False
+    BUILD_ABSTRACTION: bool = True
     # construct a sample rwo player game and wrap it to construct its symbolic version
     # transition_graph = get_graph(print_flag=True)
     # build_symbolic_model(transition_graph)
 
-    domain_file_path = PROJECT_ROOT + "/pddl_files/example_pddl/domain.pddl"
-    problem_file_path = PROJECT_ROOT + "/pddl_files/example_pddl/problem.pddl"
+    domain_file_path = PROJECT_ROOT + "/pddl_files/grid_world/domain.pddl"
+    problem_file_path = PROJECT_ROOT + "/pddl_files/grid_world/problem10_10.pddl"
 
     cudd_manager = Cudd()
-    task, domain, curr_state, next_state = create_symbolic_causal_graph(cudd_manager=cudd_manager,
-                                                                        problem_pddl_file=problem_file_path,
-                                                                        domain_pddl_file=domain_file_path)
 
-    sym_tr = SymbolicTransitionSystem(curr_states=curr_state,
-                                      next_states=next_state,
-                                      task=task,
-                                      domain=domain,
-                                      manager=cudd_manager)
-    sym_tr.create_transition_system(verbose=True, plot=False)
+    if BUILD_DFA:
+        # list of formula
+        formulas = ['F(l1 & F(l2 & F(l3 & F(l4))))']
 
-    print('Inital states: ', sym_tr.sym_init_states)
-    print('Goal states: ', sym_tr.sym_goal_states)
+
+        # create a list of DFAs
+        DFA_list = []
+
+        # for now dfa_num is zero. If oyu rhave multiple DFAs then loop over them and update DFA_num
+        for _idx, fmla in enumerate(formulas):
+            # create boolean variables
+            curr_state, next_state, _dfa = create_symbolic_dfa_graph(cudd_manager=cudd_manager, formula= fmla, dfa_num=_idx)
+            # create TR corresponding to each DFA - dfa name is only used dumping graph 
+            dfa_tr = SymbolicDFA(curr_states=curr_state, next_states=next_state, manager=cudd_manager, dfa=_dfa, dfa_name=f'dfa_{_idx}')
+            dfa_tr.create_dfa_transition_system(verbose=True, plot=True)
+
+        # sys.exit(-1)
     
-    giant_tr = reduce(lambda a, b: a | b, sym_tr.sym_tr_actions) 
+    if BUILD_ABSTRACTION:
+        task, domain, curr_state, next_state = create_symbolic_causal_graph(cudd_manager=cudd_manager,
+                                                                            problem_pddl_file=problem_file_path,
+                                                                            domain_pddl_file=domain_file_path)
+
+        sym_tr = SymbolicTransitionSystem(curr_states=curr_state,
+                                        next_states=next_state,
+                                        task=task,
+                                        domain=domain,
+                                        manager=cudd_manager)
+        sym_tr.create_transition_system(verbose=False, plot=False)
+
+        print('Inital states: ', sym_tr.sym_init_states)
+        print('Goal states: ', sym_tr.sym_goal_states)
+        
+        giant_tr = reduce(lambda a, b: a | b, sym_tr.sym_tr_actions) 
 
     # let do graph search from init state to a goal state
-
     graph_search = SymbolicSearch(init=sym_tr.sym_init_states,
                                   target=sym_tr.sym_goal_states,
                                   manager=cudd_manager,
@@ -358,7 +465,7 @@ if __name__ == "__main__":
                                   transition_func=giant_tr,
                                   sym_to_state=sym_tr.predicate_sym_map_curr.inv)
     
-    action_list = graph_search.symbolic_bfs(verbose=True)
+    action_list = graph_search.symbolic_bfs(verbose=False)
 
     # graph_search(init=sym_tr.sym_init_states,
     #              target=sym_tr.sym_goal_states,
