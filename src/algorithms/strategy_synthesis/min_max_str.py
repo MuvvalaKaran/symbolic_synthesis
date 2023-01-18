e3064713948ceaa57c0d7509abf19fee164ffa7e
Muvvalakaran <karanmuvvala@yahoo.in>
2023-01-17 19:04:57 -0700

    WIP: Adding correct fixed point computation code with state action paint. Need to implemet Min-Max operation.

--- src/algorithms/strategy_synthesis/min_max_str.py
+++ src/algorithms/strategy_synthesis/min_max_str.py
@@ -137,6 +137,41 @@ class AdversarialGame(BaseSymbolicSearch):
             print(f"([{_ts_name}], {_dfa_name})")
     
 
+    def get_prod_state_act_from_dd(self, dd_func: ADD, **kwargs) -> None:
+        """
+         This method overrides the base method by return the Actual state name using the
+          pred int map dictionary rather than the state tuple. 
+        """
+        tmp_dd_func: BDD = dd_func.bddPattern()
+        prod_cube_string: List[BDD] = self.convert_prod_cube_to_func(dd_func=tmp_dd_func, prod_curr_list=kwargs['prod_curr_list']) 
+        for prod_cube in prod_cube_string:
+            # convert the cube back to 0-1 ADD
+            tmp_prod_cube: ADD = prod_cube.toADD()
+            # get the state ADD
+            _ts_dd: ADD = tmp_prod_cube.existAbstract(self.dfa_xcube & self.sys_env_cube)
+            _ts_tuple = self.ts_handle.get_state_tuple_from_sym_state(_ts_dd, kwargs['sym_lbl_cubes'])
+            _ts_name = self.ts_handle.get_state_from_tuple(_ts_tuple)
+            assert _ts_name is not None, "Couldn't convert TS Cube to its corresponding State. FIX THIS!!!"
+
+            _dfa_name = self._look_up_dfa_name(prod_dd=tmp_prod_cube,
+                                               dfa_dict=self.dfa_sym_to_curr_state_map,
+                                               ADD_flag=False,
+                                               **kwargs)
+            
+            # get the Robot action ADD
+            ract: ADD = prod_cube.existAbstract(self.prod_xcube & self.ts_obs_cube & self.env_cube)
+            ract_name = self.ts_sym_to_robot_act_map[ract]
+
+            # rct_cubes = self.convert_prod_cube_to_func(dd_func=ract, prod_curr_list=self.sys_act_vars)
+
+            hact: ADD = prod_cube.existAbstract(self.prod_xcube & self.ts_obs_cube & self.sys_cube)
+            hact_cubes = self.convert_prod_cube_to_func(dd_func=hact, prod_curr_list=self.env_act_vars)
+            for dd in hact_cubes:
+                hact_name =  self.ts_sym_to_human_act_map[dd]
+            
+                print(f"({_ts_name}, {_dfa_name})  ----{ract_name} & {hact_name}")
+    
+
     def get_pre_states(self, ts_action: List[BDD], From: BDD, prod_curr_list=None) -> BDD:
         """
          Compute the predecessors using the compositional approach. From is a collection of 0-1 ADD.
@@ -158,8 +193,12 @@ class AdversarialGame(BaseSymbolicSearch):
          First loop over all the edges in the graph and compute all the possible state values. As the states are reachable from the initial state, 
           all the state should have finite (not infinity) non-zero values except for the accepting state. 
         """
-        open_list = {}
+        open_list = defaultdict(lambda: self.manager.addZero())
+        # keep tracks all the states action pairs we have visited till now
         closed = self.manager.addZero()
+        # computes the max of state action pair max(s, as, -)
+        closed_max_min_add: ADD = self.manager.plusInfinity()
+
         c_max = self._get_max_tr_action_cost()
 
         # counter used for breaking
@@ -177,12 +216,12 @@ class AdversarialGame(BaseSymbolicSearch):
         prod_curr_list.extend([lbl for sym_vars_list in self.ts_obs_list for lbl in sym_vars_list])
         prod_curr_list.extend(self.ts_x_list)
 
+        prod_curr_act_list = prod_curr_list + self.sys_act_vars + self.env_act_vars
+
         prod_bdd_curr_list = [_avar.bddPattern() for _avar in prod_curr_list]
+        prod_curr_bdd_act_list = [_avar.bddPattern() for _avar in prod_curr_act_list]
 
         sym_lbl_cubes = self._create_lbl_cubes()
-
-        # if verbose:
-        #     self.get_prod_states_from_dd(dd_func=self.winning_states[0], sym_lbl_cubes=sym_lbl_cubes, prod_curr_list=prod_bdd_curr_list)
         
         while not open_list[g_layer].isZero():
             # remove all states that have been explored
@@ -198,27 +237,26 @@ class AdversarialGame(BaseSymbolicSearch):
                 # Add states to be expanded next to already expanded states
                 closed |= open_list[g_layer]
 
+                curr_bucket: ADD = open_list[g_layer].existAbstract(self.sys_env_cube)
+                curr_bucket_bdd: BDD = curr_bucket.bddPattern()
                 if verbose:
                     print("Computing pre of:")
-                    self.get_prod_states_from_dd(dd_func=open_list[g_layer], sym_lbl_cubes=sym_lbl_cubes, prod_curr_list=prod_bdd_curr_list)
+                    self.get_prod_states_from_dd(dd_func=curr_bucket, sym_lbl_cubes=sym_lbl_cubes, prod_curr_list=prod_bdd_curr_list)
 
+                
                 for tr_idx, tr_action in enumerate(self.ts_transition_fun_list):
                     # first get the corresponding transition action cost (constant at the terminal node)
                     curr_act_name: str = self.ts_action_idx_map.inv[tr_idx]
                     if verbose:
                         print(f"Pre under Action: {curr_act_name}")
                     
-                    # action_cost = tr_action[0].findMax()
                     action_cost: ADD =  self.ts_handle.weight_dict[curr_act_name]
                     act_val: int =  int(re.findall(r'\d+', action_cost.__repr__())[0])
-
-                    # step = g_val + action_cost
-                    # step_val = int(re.findall(r'\d+', step.__repr__())[0])
                     step_val = g_layer + act_val
 
                     # compute preimages
                     tr_action_bdd: List[BDD] = [ele.bddPattern() for ele in tr_action]
-                    pred_prod = self.get_pre_states(ts_action=tr_action_bdd, From=open_list[g_layer].bddPattern(), prod_curr_list=prod_bdd_curr_list)
+                    pred_prod = self.get_pre_states(ts_action=tr_action_bdd, From=curr_bucket_bdd, prod_curr_list=prod_bdd_curr_list)
 
                     if pred_prod.isZero():
                         continue
@@ -232,9 +270,14 @@ class AdversarialGame(BaseSymbolicSearch):
                     # TODO: Add which action caused this transition to to the BDD before adding it the bucket
                     # if the bucket exists then take the union else initialize the bucket
                     if step_val in open_list:
-                        open_list[step_val] |= prod_image_restricted.existAbstract(self.sys_env_cube)
+                        open_list[step_val] |= prod_image_restricted 
                     else:
-                        open_list[step_val] = prod_image_restricted.existAbstract(self.sys_env_cube)
+                        open_list[step_val] = prod_image_restricted
+                    
+
+                    if verbose:
+                        print(f"*****************Max states-action value under action {curr_act_name} is {step_val}****************************")
+                        self.get_prod_states_from_dd(dd_func=prod_image_restricted.existAbstract(self.sys_env_cube), sym_lbl_cubes=sym_lbl_cubes, prod_curr_list=prod_bdd_curr_list)
             
             else:
                 empty_bucket_counter += 1
@@ -245,6 +288,16 @@ class AdversarialGame(BaseSymbolicSearch):
             
             # g_val = g_val + self.manager.addOne()
             g_layer += 1
+
+            # compute the max of state and action pair
+            closed_max_add = closed_max_add.max(open_list[g_layer].ite(self.manager.addConst(g_layer), self.manager.addZero()))
+            
+            # if verbose:
+            #     print(f"*****************Max states-action value at layer {g_layer}****************************")
+            #     # self.get_prod_state_act_from_dd(dd_func=open_list[g_layer].existAbstract(self.sys_env_cube),
+            #     #                                 sym_lbl_cubes=sym_lbl_cubes,
+            #     #                                 prod_curr_list=prod_curr_bdd_act_list)
+            #     self.get_prod_states_from_dd(dd_func=open_list[g_layer].existAbstract(self.sys_env_cube), sym_lbl_cubes=sym_lbl_cubes, prod_curr_list=prod_bdd_curr_list)
 
             if g_layer > max(open_list.keys()):
                 g_layer -= 1
